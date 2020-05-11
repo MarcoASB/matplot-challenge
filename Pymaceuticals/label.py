e2787e32ab5905e521d6e449776b4244555a9822
Marco Solorzano Baez <marsolbae@gmail.com>
2020-05-10 23:24:51 -0500

    Functions

---
+++ Pymaceuticals/label.py
@@ -0,0 +1,15 @@
+# Use functions to label the percentages of changes
+
+def autolabelFail(rects):
+    for rect in rects:
+        height = rect.get_height()
+        ax.text(rect.get_x() + rect.get_width()/2., 3,
+                '%d%%' % int(height),
+                ha='center', va='bottom', color="white")
+
+def autolabelPass(rects):
+    for rect in rects:
+        height = rect.get_height()
+        ax.text(rect.get_x() + rect.get_width()/2., -8,
+                '-%d%% ' % int(height),
+                ha='center', va='bottom', color="white")
